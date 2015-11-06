4ba96a1a1ef276f564078e202ecde798d87aab59
Ilia Grigorev <ilyaicegrigoryev@gmail.com>
2015-11-06 14:35:53 +0100

    Changed glasses texture minification filter to linear

--- app/src/main/java/com/limelight/GlassesRenderer.java
+++ app/src/main/java/com/limelight/GlassesRenderer.java
@@ -79,7 +79,7 @@ public class GlassesRenderer implements GLSurfaceView.Renderer {
         textureId = textures[0];
         GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textureId);
         GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
-                GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+                GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
         GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
                 GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
         GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
